2e5dacf57222e10c430c4725f3421f9fbfc53237
Ivy Patterson <105239600+ivy-p@users.noreply.github.com>
2022-10-03 17:40:22 -0600

    Basic styles and content applied

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const moreButton = document.querySelector(".more-button");
+const moreInfo = document.querySelector(".about--more-info");
+const mainDescription = document.querySelector(".about--description");
+const profileImage = document.querySelector(".profile-img")
+
+moreButton.addEventListener("click", removeHide)
+
+function removeHide () {
+
+    if (moreInfo.classList.contains("hide")) {
+        moreInfo.classList.remove("hide");
+        moreButton.innerText = "Business in the front:"
+        mainDescription.classList.add("hide");
+        profileImage.classList.add("hide");
+    } else {
+        moreInfo.classList.add("hide")
+        moreButton.innerText = "Party in the back:"
+        mainDescription.classList.remove("hide");
+        profileImage.classList.remove("hide");
+    }
+}
