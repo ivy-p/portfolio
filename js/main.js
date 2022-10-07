faae1652ab4747590ef43ac0c01219744cd9a5f0
Ivy Patterson <105239600+ivy-p@users.noreply.github.com>
2022-10-07 09:37:49 -0600

    Minor changes

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const moreInfo = document.querySelector(".about--more-info")
 const profileImage = document.querySelector(".profile-img");
 const aboutDescription = document.querySelector(".about--description");
 const aboutContainer = document.getElementById("about");
+const infoContainer = document.querySelector(".info-container")
 
 //Adding event listener for About section button
 moreButton.addEventListener("click", removeHide)
@@ -20,6 +21,7 @@ function removeHide () {
         moreButton.innerText = "Party in the back:"
         aboutDescription.classList.remove("hide");
         profileImage.classList.remove("hide");
+        infoContainer.style.animationDuration = "1s"
     }
 }
 
